b25e4f111ba8683adc9e6bb86cdbb1fead520b6d
Nadir <nadir.azza@hotmail.be>
2023-07-30 21:22:47 +0200

    Ajout annotation crossOrigin panier controller

--- src/main/java/com/example/controller/PanierController.java
+++ src/main/java/com/example/controller/PanierController.java
@@ -10,14 +10,13 @@ import com.example.service.UtilisateurService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
+
 public class PanierController {
     @Autowired
     private PanierService panierService;
