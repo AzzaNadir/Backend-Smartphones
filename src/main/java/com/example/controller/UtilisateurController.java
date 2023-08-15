3259a9eba52e4660914b3f6bb972c04185a7c115
Nadir <nadir.azza@hotmail.be>
2023-08-16 01:07:38 +0200

    Profile

--- src/main/java/com/example/controller/UtilisateurController.java
+++ src/main/java/com/example/controller/UtilisateurController.java
@@ -1,11 +1,14 @@
 package com.example.controller;
 
+import com.example.configuration.JwtTokenUtil;
 import com.example.model.TypeUtilisateur;
 import com.example.model.Utilisateur;
 import com.example.service.UtilisateurService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/utilisateurs")
@@ -15,6 +18,9 @@ public class UtilisateurController {
 
     @Autowired
     private UtilisateurService utilisateurService;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
     @GetMapping("/hello")
     public String helloworld() {
 
@@ -30,6 +36,34 @@ public class UtilisateurController {
         } catch (RuntimeException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @PutMapping("/UpdateProfile")
+    public ResponseEntity<String> modifierProfil(HttpServletRequest request, @RequestBody Utilisateur utilisateurModifie) {
+       Utilisateur utilisateur = utilisateurService.getUtilisateurFromToken(request);
+
+        utilisateur.setNom(utilisateurModifie.getNom());
+        utilisateur.setPrenom(utilisateurModifie.getPrenom());
+        utilisateur.setAdresse(utilisateurModifie.getAdresse());
+        utilisateur.setNumeroDeTelephone(utilisateurModifie.getNumeroDeTelephone());
+
+        utilisateurService.enregistrerUtilisateur(utilisateur);
+
+        return ResponseEntity.ok("Profil utilisateur mis à jour avec succès");
+    }
+
+    @PutMapping("/changer-mot-de-passe")
+    public ResponseEntity<String> changerMotDePasse(HttpServletRequest request, @RequestParam String ancienMotDePasse, @RequestParam String nouveauMotDePasse) {
+        Utilisateur utilisateur = utilisateurService.getUtilisateurFromToken(request);
+
+        if (!utilisateur.getMotDePasse().equals(ancienMotDePasse)) {
+            return ResponseEntity.badRequest().body("Le mot de passe actuel est incorrect");
+        }
+
+        utilisateur.setMotDePasse(nouveauMotDePasse);
+        utilisateurService.enregistrerUtilisateur(utilisateur);
+
+        return ResponseEntity.ok("Mot de passe modifié avec succès");
     }
 }
 
--- src/main/java/com/example/service/UtilisateurService.java
+++ src/main/java/com/example/service/UtilisateurService.java
@@ -1,5 +1,6 @@
 package com.example.service;
 
+import com.example.configuration.JwtTokenUtil;
 import com.example.model.TypeUtilisateur;
 import com.example.model.Utilisateur;
 import com.example.repository.UtilisateurRepository;
@@ -8,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -18,8 +20,10 @@ public class UtilisateurService {
     private UtilisateurRepository utilisateurRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
-    public Utilisateur creerAdmin(String nom, String prenom, String email, String motDePasse, String adresse,String numeroDeTelephone) {
+    public Utilisateur creerAdmin(String nom, String prenom, String email, String motDePasse, String adresse, String numeroDeTelephone) {
         Utilisateur admin = utilisateurRepository.findByType(TypeUtilisateur.ADMINISTRATEUR);
         if (admin == null) {
             admin = new Utilisateur();
@@ -37,7 +41,7 @@ public class UtilisateurService {
     }
 
     public void registerUser(Utilisateur user) {
-       // Vérifier si l'utilisateur existe déjà dans la base de données
+        // Vérifier si l'utilisateur existe déjà dans la base de données
         if (utilisateurRepository.findByEmail(user.getEmail()) != null) {
             throw new RuntimeException("Email déjà utilisé");
         }
@@ -50,6 +54,9 @@ public class UtilisateurService {
         utilisateurRepository.save(user);
     }
 
+    public void enregistrerUtilisateur(Utilisateur utilisateur) {
+        utilisateurRepository.save(utilisateur);
+    }
 
     public Utilisateur getUtilisateurById(Long id) {
         Optional<Utilisateur> optionalUtilisateur = utilisateurRepository.findById(id);
@@ -59,12 +66,24 @@ public class UtilisateurService {
             throw new NoSuchElementException("Utilisateur with ID " + id + " does not exist.");
         }
     }
+
     public Utilisateur getUtilisateurParEmail(String email) {
         return utilisateurRepository.findByEmail(email);
     }
+
+
+    public Utilisateur getUtilisateurFromToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        String emailUtilisateur = jwtTokenUtil.getUsernameFromToken(token.substring(7));
+
+        // Ensuite, utilisez le service ou le référentiel pour trouver l'utilisateur par adresse e-mail
+        Utilisateur utilisateur = getUtilisateurParEmail(emailUtilisateur);
+        if (utilisateur == null) {
+            throw new RuntimeException("L'utilisateur n'existe pas !");
+        }
+        return utilisateur;
+    }
 }
-
-
 
 
 
