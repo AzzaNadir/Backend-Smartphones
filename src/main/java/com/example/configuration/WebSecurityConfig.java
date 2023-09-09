0ee426fdf10c35e967b3591109be875ea25bb2d5
Nadir <nadir.azza@hotmail.be>
2023-09-09 23:52:42 +0200

    Delete comments

--- src/main/java/com/example/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/configuration/WebSecurityConfig.java
@@ -33,9 +33,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // configure AuthenticationManager so that it knows from where to load
-        // user for matching credentials
-        // Use BCryptPasswordEncoder
         auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
@@ -52,22 +49,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        // We don't need CSRF for this example
         httpSecurity
                 .csrf().disable()
                 .cors().and() // enable CORS
-                // dont authenticate this particular request
-//                .cors().and().csrf().and()
-                .authorizeRequests().antMatchers("/api/authenticate", "/api/AfficherSmartphones", "/api/SmartphonesFiltre","/api/utilisateurs/register","/api/Smartphones/{id}","/api/Smartphones/{id}/availableOptions","/api/GetSmartphonesByCaracteristiques","/checkout/success","/api/request-password","/api/reset-password").permitAll().
-
-                // all other requests need to be authenticated
-
-                        anyRequest().authenticated().and().
-                // make sure we use stateless session; session won't be used to
-                // store user's state.
-                        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        // Add a filter to validate the tokens with every request
+                .authorizeRequests().antMatchers("/api/authenticate", "/api/AfficherSmartphones", "/api/SmartphonesFiltre", "/api/utilisateurs/register", "/api/Smartphones/{id}", "/api/Smartphones/{id}/availableOptions", "/api/GetSmartphonesByCaracteristiques", "/checkout/success", "/api/request-password", "/api/reset-password").permitAll().
+                anyRequest().authenticated().and().
+                exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
